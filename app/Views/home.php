e183dea812c15cf3f716d99dc36ed3a641b97dcb
Rafael Almeida <rafael13almeida@gmail.com>
2022-06-05 08:28:35 -0300

    Refatorando as views

--- app/Views/home.php
+++ app/Views/home.php
@@ -22,18 +22,3 @@
 
 </html>
 
-<!-- 
-'<!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>POO</title>
-        </head>
-        <body>
-            <h1>Programação Orientada a Objetos</h1>
-            <p>Lista</p>
-            <ul>'.$listaHTML.'</ul>
-        </body>
-        </html>'; -->
